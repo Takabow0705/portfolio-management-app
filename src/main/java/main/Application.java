2b63893b19f8878c9eb5d691967fa4be151d1220
Takahiro Haraguchi <hrgc.takahiro@outlook.jp>
2019-07-10 15:07:37 +0900

    add EnableConfiguration class

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -2,11 +2,12 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan
 public class Application {
     public static void main(String[] args){
