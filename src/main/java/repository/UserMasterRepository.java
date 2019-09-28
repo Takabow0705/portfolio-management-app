d20c2ec2eb691180eafbe8fc8140d9c3060b4732
takahiro <tkhr.px07q05@gmail.com>
2019-09-29 02:03:57 +0900

    restruct dir structure

--- src/main/java/repository/UserMasterRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import commons.entities.UserMaster;
-
-@Repository
-public interface UserMasterRepository extends JpaRepository<UserMaster, Long>{}
