f9d7019b76fadc4e2f75880b3a4a39d111da62db
takahiro <tkhr.px07q05@gmail.com>
2019-10-05 21:36:07 +0900

    remove unused files

--- src/main/java/app/component/controller/UserMasterManagementController.java
+++ src/main/java/app/component/controller/UserMasterManagementController.java
@@ -3,7 +3,6 @@ package app.component.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
