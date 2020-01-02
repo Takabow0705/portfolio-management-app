2594e8f3b24fd8b20ed988bc64ce803f3d2fd8cd
Takabow0705 <hrgc.takahiro@outlook.jp>
2020-01-02 22:23:35 +0900

    add JSON serialize settings

--- src/main/java/app/commons/entities/user/UserAuthInfo.java
+++ src/main/java/app/commons/entities/user/UserAuthInfo.java
@@ -1,32 +1,61 @@
 package app.commons.entities.user;
 
 import app.commons.enums.UserAuthentication;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.context.annotation.SessionScope;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Collection;
 
-public class UserAuthInfo implements UserDetails {
+@SessionScope
+@JsonSerialize
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserAuthInfo implements UserDetails, Serializable {
 
-    /** ユーザ名はメールアドレスで代用*/
+    /**
+     * serial version UID
+     */
+    private static final Long serialVersionUID = 1L;
+
+    /**
+     * ユーザID
+     */
+    private Long userId;
+    /**
+     * ユーザ名はメールアドレスで代用
+     */
     private String userMailAddress;
-    /** パスワード*/
+    /**
+     * パスワード
+     */
     private String password;
-    /** ロックの有無*/
+    /**
+     * ロックの有無
+     */
     private boolean isLocked;
-    /** 削除の有無*/
+    /**
+     * 削除の有無
+     */
     private boolean isDeleted;
-    /** ユーザの権限*/
+    /**
+     * ユーザの権限
+     */
     private Collection<UserAuthentication> authorities;
 
-
-    public UserAuthInfo(UserMaster userMaster){
+    public UserAuthInfo(UserMaster userMaster) {
+        this.userId = userMaster.getUserId();
         this.userMailAddress = userMaster.getMailAddress();
         this.password = userMaster.getPassword();
         this.isDeleted = userMaster.isDeleted();
         this.isLocked = userMaster.isLocked();
         this.authorities = Arrays.asList(userMaster.getUserAuthentication());
+    }
+
+    public UserAuthInfo() {
     }
 
     public void setAuthorities(Collection<UserAuthentication> authorities) {
@@ -66,5 +95,41 @@ public class UserAuthInfo implements UserDetails {
     @Override
     public boolean isEnabled() {
         return !this.isDeleted;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserMailAddress() {
+        return userMailAddress;
+    }
+
+    public void setUserMailAddress(String userMailAddress) {
+        this.userMailAddress = userMailAddress;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void setLocked(boolean locked) {
+        isLocked = locked;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
     }
 }
