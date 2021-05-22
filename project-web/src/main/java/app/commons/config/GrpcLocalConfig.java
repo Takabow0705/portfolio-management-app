24a9d0725b73636f52a946e40b9cfd53f69c9079
Takabow0705 <hrgc.takahiro@outlook.jp>
2021-05-22 23:08:58 +0900

    refactor env variables

--- project-web/src/main/java/app/commons/config/GrpcLocalConfig.java
+++ project-web/src/main/java/app/commons/config/GrpcLocalConfig.java
@@ -4,6 +4,7 @@ import app.commons.properties.GrpcCalculatorProperty;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,10 +16,11 @@ public class GrpcLocalConfig {
     @Autowired
     private GrpcCalculatorProperty property;
 
-    @Bean(name = "calculator")
+    @Bean
+    @Qualifier("calculator")
     public ManagedChannel managedChannel(){
         String hostname = property.getHostname();
-        int port = Integer.parseInt(property.getPort());
+        int port = property.getPort();
         return ManagedChannelBuilder.forAddress(hostname, port)
                 .usePlaintext()
                 .build();
