612675cd7b9bf8563eedf9b373d8f9ef68e7b87a
Takabow0705 <hrgc.takahiro@outlook.jp>
2021-08-09 12:21:52 +0900

    delete unused class

--- project-infra/src/main/java/project/infra/rdb/stockportfolioevaluation/StockPortfolioEvaluationRepository.java
+++ project-infra/src/main/java/project/infra/rdb/stockportfolioevaluation/StockPortfolioEvaluationRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StockPortfolioEvaluationRepository extends JpaRepository<StockPortfolioEvaluation, Long>, StockPortfolioEvaluationRepositoryCustom{
+public interface StockPortfolioEvaluationRepository extends JpaRepository<StockPortfolioEvaluation, Long> {
 }
--- project-infra/src/main/java/project/infra/rdb/stockportfolioevaluation/StockPortfolioEvaluationRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package project.infra.rdb.stockportfolioevaluation;
-
-import java.util.List;
-
-public interface StockPortfolioEvaluationRepositoryCustom {
-     public abstract void bulkInsert(List<StockPortfolioEvaluation> stockPortfolioEvaluationList);
-}
