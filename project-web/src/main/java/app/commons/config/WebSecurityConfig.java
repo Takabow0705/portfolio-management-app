9dc144ec0691e8a241db97373d777da415445882
Takabow0705 <hrgc.takahiro@outlook.jp>
2021-09-04 00:48:49 +0900

    create authentication  filter for SPA

--- project-web/src/main/java/app/commons/config/WebSecurityConfig.java
+++ project-web/src/main/java/app/commons/config/WebSecurityConfig.java
@@ -1,23 +1,23 @@
 package app.commons.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import app.commons.filter.JsonApiAuthenticationFilter;
+import app.component.service.UserAuthDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private UserDetailsService userAuthDetailService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,10 +25,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().ignoringAntMatchers("/api/**");
         //デフォルトでアクセス許可されている
         http.authorizeRequests()
+            .antMatchers("/api/login").permitAll()
             .antMatchers( "/login").permitAll()
             .antMatchers("/js/**").permitAll()
             .antMatchers("/*.css").permitAll()
             .anyRequest().authenticated();
+        http.addFilterAt(jsonApiAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         //ログイン成功時の動作
         http.formLogin()
             .loginPage("/login").defaultSuccessUrl("/")
@@ -36,21 +38,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         //ログアウト時の動作
         http.logout()
             .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-            .deleteCookies("JSESSIONID")
+            .deleteCookies("SESSION")
             .invalidateHttpSession(true)
             .logoutSuccessUrl("/login")
             .permitAll();
     }
 
+    /**
+     * SPA用の認証Filter
+     * @return
+     */
+    @Bean
+    public JsonApiAuthenticationFilter jsonApiAuthenticationFilter() throws Exception{
+        JsonApiAuthenticationFilter jsonApiAuthenticationFilter = new JsonApiAuthenticationFilter(authenticationManager());
+        jsonApiAuthenticationFilter.setAuthenticationSuccessHandler((req, res, ex) -> res.setStatus(HttpServletResponse.SC_OK));
+        jsonApiAuthenticationFilter.setAuthenticationFailureHandler((req, res, ex) -> res.setStatus(HttpServletResponse.SC_UNAUTHORIZED));
+        jsonApiAuthenticationFilter.setAllowSessionCreation(true);
+        return jsonApiAuthenticationFilter;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth
-                .userDetailsService(userAuthDetailService)
-                .passwordEncoder(passwordEncoder());
+        auth.userDetailsService(userAuthDetailService()).passwordEncoder(passwordEncoder());
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserAuthDetailService userAuthDetailService(){
+        return new UserAuthDetailService();
     }
 }
--- project-web/src/main/java/app/component/service/UserAuthDetailService.java
+++ project-web/src/main/java/app/component/service/UserAuthDetailService.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
 public class UserAuthDetailService implements UserDetailsService {
 
     private static final FluentLogger logger = FluentLogger.forEnclosingClass();
