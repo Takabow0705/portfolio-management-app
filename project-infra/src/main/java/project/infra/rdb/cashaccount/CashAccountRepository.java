a804be41eb15ad4ce32167a542b7da08108538be
Takabow0705 <hrgc.takahiro@outlook.jp>
2021-10-03 22:18:36 +0900

    add annotation

--- project-infra/src/main/java/project/infra/rdb/cashaccount/CashAccountRepository.java
+++ project-infra/src/main/java/project/infra/rdb/cashaccount/CashAccountRepository.java
@@ -1,6 +1,8 @@
 package project.infra.rdb.cashaccount;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CashAccountRepository extends JpaRepository<CashAccount, Long> {
 }
